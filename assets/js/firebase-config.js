6562ac97dbd4519e6182d709650fdd1be3dfd066
Shantanu Guin <shantanu.guin281203@gmail.com>
2026-03-03 12:13:34 +0300

    feat: add firebase-config.js for reuse

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,10 @@
+// --- Firebase Configuration ---
+const firebaseConfig = {
+    apiKey: "AIzaSyCayfWGQP1t3oBU9eFj-3Ww0uu7nSl3Q4g",
+    authDomain: "sidneymailer.firebaseapp.com",
+    projectId: "sidneymailer",
+    storageBucket: "sidneymailer.firebasestorage.app",
+    messagingSenderId: "911316068119",
+    appId: "1:911316068119:web:ce23dbe663dc385a81f952",
+    measurementId: "G-NJ0QH6DEXG"
+};
